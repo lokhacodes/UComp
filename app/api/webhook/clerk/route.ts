7521e8a1699743b3411e2c1b14eb0e11165633c2
lokhacodes <sumaiyasonali421@gmail.com>
2025-08-17 13:15:58 +0600

    MongodbUser

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: Request) {
       username: username!,
       firstName: first_name || '',
       lastName: last_name || '',
-      photo: image_url,
+      //photo: image_url,
     }
 
     const newUser = await createUser(user)
@@ -75,7 +75,7 @@ export async function POST(req: Request) {
       firstName: first_name || '',
       lastName: last_name || '',
       username: username!,
-      photo: image_url,
+      //photo: image_url,
     }
 
     const updatedUser = await updateUser(id, user)
