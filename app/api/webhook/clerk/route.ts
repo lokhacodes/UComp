3faee5ef8f4ce3272ac8cf0d31b036ce0f1620e7
lokhacodes <sumaiyasonali421@gmail.com>
2025-08-17 14:17:22 +0600

    Your commit message here

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -65,7 +65,7 @@ export async function POST(req: Request) {
       username: username!,
       firstName: first_name || '',
       lastName: last_name || '',
-      //photo: image_url,
+      photo: image_url,
     }
 
     const newUser = await createUser(user)
@@ -80,7 +80,7 @@ export async function POST(req: Request) {
       firstName: first_name || '',
       lastName: last_name || '',
       username: username!,
-      //photo: image_url,
+      photo: image_url,
     }
 
     const updatedUser = await updateUser(id, user)
--- types/index.ts
+++ types/index.ts
@@ -5,14 +5,14 @@ export type CreateUserParams = {
   lastName: string
   username: string
   email: string
-//  photo: string
+  photo: string
 }
 
 export type UpdateUserParams = {
   firstName: string
   lastName: string
   username: string
-//  photo: string
+  photo: string
 }
 
 // ====== EVENT PARAMS
