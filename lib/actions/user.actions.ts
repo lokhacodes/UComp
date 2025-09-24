750641c7b5611da23bd79d64c2f1478f23c32005
lokhacodes <sumaiyasonali421@gmail.com>
2025-09-24 11:25:32 +0600

    exchnage

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,10 +9,10 @@ import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
 
-// CREATE
 export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase()
+
     const newUser = await User.create(user)
     return JSON.parse(JSON.stringify(newUser))
   } catch (error) {
@@ -20,11 +20,12 @@ export async function createUser(user: CreateUserParams) {
   }
 }
 
-// READ
-export async function getUserById(clerkId: string) {
+export async function getUserById(userId: string) {
   try {
     await connectToDatabase()
-    const user = await User.findOne({ clerkId })
+
+    const user = await User.findById(userId)
+
     if (!user) throw new Error('User not found')
     return JSON.parse(JSON.stringify(user))
   } catch (error) {
@@ -32,11 +33,12 @@ export async function getUserById(clerkId: string) {
   }
 }
 
-// UPDATE
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
     await connectToDatabase()
+
     const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+
     if (!updatedUser) throw new Error('User update failed')
     return JSON.parse(JSON.stringify(updatedUser))
   } catch (error) {
@@ -44,24 +46,27 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
   }
 }
 
-// DELETE
 export async function deleteUser(clerkId: string) {
   try {
     await connectToDatabase()
 
+    // Find user to delete
     const userToDelete = await User.findOne({ clerkId })
-    if (!userToDelete) throw new Error('User not found')
+
+    if (!userToDelete) {
+      throw new Error('User not found')
+    }
 
     // Unlink relationships
     await Promise.all([
+      // Update the 'events' collection to remove references to the user
       Event.updateMany(
         { _id: { $in: userToDelete.events } },
         { $pull: { organizer: userToDelete._id } }
       ),
-      Order.updateMany(
-        { _id: { $in: userToDelete.orders } },
-        { $unset: { buyer: 1 } }
-      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
     ])
 
     // Delete user
